a6572c0d13cad9395cd74110606a361c899c18ed
Damian Piętka <damianpietka@gmail.com>
2021-02-28 17:02:36 +0100

    search functionality and some styling

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -2,6 +2,26 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+// svg
+import clear_sky_d from '../img/clear-sky-d.svg';
+import clear_sky_n from '../img/clear-sky-n.svg';
+import few_clouds_d from '../img/few-clouds-d.svg';
+import few_clouds_n from '../img/few-clouds-n.svg';
+import clouds_d from '../img/clouds-d.svg';
+import clouds_n from '../img/clouds-n.svg';
+import broken_clouds_d from '../img/broken-clouds-d.svg';
+import broken_clouds_n from '../img/broken-clouds-n.svg';
+import shower_rain_d from '../img/shower-rain-d.svg';
+import shower_rain_n from '../img/shower-rain-n.svg';
+import rain_d from '../img/rain-d.svg';
+import rain_n from '../img/rain-n.svg';
+import thunder_d from '../img/thunder-d.svg';
+import thunder_n from '../img/thunder-n.svg';
+import snow_d from '../img/snow-d.svg';
+import snow_n from '../img/snow-n.svg';
+import mist_d from '../img/mist-d.svg';
+import mist_n from '../img/mist-n.svg';
+
 const MainCard = ({ weatherToday, setWeatherToday }) => {
   //const [weatherToday, setWeatherToday] = useState(0);
   const [yourCity, setYourCity] = useState('');
@@ -16,7 +36,7 @@ const MainCard = ({ weatherToday, setWeatherToday }) => {
     setIsLoading(true);
     axios
       .get(
-        `http://api.openweathermap.org/data/2.5/forecast?q=Warszawa&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
+        `http://api.openweathermap.org/data/2.5/forecast?q=${yourCity}&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
       )
       .then((resp) => {
         setWeatherToday(resp.data);
@@ -34,7 +54,7 @@ const MainCard = ({ weatherToday, setWeatherToday }) => {
   if (!weatherToday) {
     return (
       <StyledCard>
-        <StyledFormDiv>
+        <StyledFormDiv onSubmit={showWeather}>
           <input
             onChange={(e) => changeHandler(e)}
             type='text'
@@ -43,18 +63,48 @@ const MainCard = ({ weatherToday, setWeatherToday }) => {
             placeholder='City'
             value={yourCity}
           />
+          <div className='button-container'>
+            <StyledButton type='submit'>Search</StyledButton>
+          </div>
         </StyledFormDiv>
-        <StyledButton onClick={showWeather}>Search</StyledButton>
       </StyledCard>
     );
   } else {
+    const getIcon = (icon) => {
+      if (icon === '01d') return clear_sky_d;
+      else if (icon === '01n') return clear_sky_n;
+      else if (icon === '02d') return few_clouds_d;
+      else if (icon === '02n') return few_clouds_n;
+      else if (icon === '03d') return clouds_d;
+      else if (icon === '03n') return clouds_n;
+      else if (icon === '04d') return broken_clouds_d;
+      else if (icon === '04n') return broken_clouds_n;
+      else if (icon === '09d') return shower_rain_d;
+      else if (icon === '09n') return shower_rain_n;
+      else if (icon === '10d') return rain_d;
+      else if (icon === '10n') return rain_n;
+      else if (icon === '11d') return thunder_d;
+      else if (icon === '11n') return thunder_n;
+      else if (icon === '13d') return snow_d;
+      else if (icon === '13n') return snow_n;
+      else if (icon === '50d') return mist_d;
+      else if (icon === '50d') return mist_n;
+      else return mist_n;
+    };
     return (
       <StyledCard type='space-around'>
         <h1>{weatherToday.city.name}</h1>
         <StyledWeatherToday>
           <p>{weatherToday.list[0].weather[0].description}</p>
-          <p>{weatherToday.list[0].weather[0].icon}</p>
-          <p>{parseInt(weatherToday.list[0].main.temp)} &deg;C</p>
+          <div className='box'>
+            <img
+              src={getIcon(weatherToday.list[0].weather[0].icon)}
+              alt={weatherToday.list[0].weather[0].description}
+            />
+            <p className='temperature'>
+              {parseInt(weatherToday.list[0].main.temp)} &deg;C
+            </p>
+          </div>
         </StyledWeatherToday>
         <StyledNextDays>
           <p>Mon</p>
@@ -74,15 +124,27 @@ const StyledCard = styled.div.attrs((props) => ({
   min-width: 50%;
   border-radius: 1rem;
   text-align: center;
-  //border: solid;
   box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.2), 0px 20px 20px rgba(0, 0, 0, 0.4);
   display: flex;
   flex-direction: column;
   justify-content: ${(props) => props.type};
+  img {
+    height: 5rem;
+    //filter: invert(95%) sepia(13%) saturate(312%) hue-rotate(112deg)
+    //  brightness(101%) contrast(116%);
+  }
 `;
 
 const StyledWeatherToday = styled.div`
   margin: 2rem;
+  .box {
+    display: flex;
+    justify-content: space-around;
+    margin: 0rem 12rem;
+  }
+  .temperature {
+    font-size: 4rem;
+  }
 `;
 
 const StyledNextDays = styled.div`
